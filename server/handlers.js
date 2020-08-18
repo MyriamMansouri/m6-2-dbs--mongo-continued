23f78d32cbf57a2b4071eb38cea0443df9bd7373
myriam mansouri <myriam.mansouri@gmail.com>
2020-08-18 15:52:52 -0400

    ## Exercise 3 - Booking a seat

--- server/handlers.js
+++ server/handlers.js
@@ -1,6 +1,7 @@
 "use strict";
 const { MongoClient } = require("mongodb");
 const assert = require("assert");
+const { stack } = require("./routes");
 
 require("dotenv").config();
 const { MONGO_URI } = process.env;
@@ -12,22 +13,30 @@ const options = {
   useUnifiedTopology: true,
 };
 
+let state = {
+  seats: {},
+};
+
 const getSeats = async (req, res) => {
-  let seats = {};
+  const seats = {};
   try {
     const client = await MongoClient(MONGO_URI, options);
     await client.connect();
     const db = client.db(DB_NAME);
     const seatsArray = await db.collection("seats").find().toArray();
+
     seatsArray.forEach((seat) => {
       seats[seat._id] = {
         price: seat.price,
         isBooked: seat.isBooked,
       };
     });
+
+    state.seats = seats;
+
     client.close();
     res.json({
-      seats: seats,
+      seats,
       numOfRows: 8,
       seatsPerRow: 12,
     });
@@ -36,4 +45,55 @@ const getSeats = async (req, res) => {
   }
 };
 
-module.exports = { getSeats };
+const bookSeat = async (req, res) => {
+  let lastBookingAttemptSucceeded = false;
+  const { seatId, creditCard, expiration } = req.body;
+
+  if (!creditCard || !expiration) {
+    return res.status(400).json({
+      status: 400,
+      message: "Please provide credit card information!",
+    });
+  }
+
+  if (lastBookingAttemptSucceeded) {
+    lastBookingAttemptSucceeded = !lastBookingAttemptSucceeded;
+
+    return res.status(500).json({
+      message: "An unknown error has occurred. Please try your request again.",
+    });
+  }
+
+  lastBookingAttemptSucceeded = !lastBookingAttemptSucceeded;
+
+  try {
+    const client = await MongoClient(MONGO_URI, options);
+    await client.connect();
+    const db = client.db(DB_NAME);
+
+    if (!state.seats[seatId].isBooked) {
+      await db
+        .collection("seats")
+        .updateOne({ _id: seatId }, { $set: {isBooked: true} });
+
+      res.status(200).json({
+        status: 200,
+        success: true,
+      });
+
+    } else {
+
+      res.status(400).json({
+        message: "This seat has already been booked!",
+      });
+
+    }
+
+    client.close();
+  } catch (err) {
+      console.log(err.stack)
+  }
+  return;
+};
+
+module.exports = { getSeats, bookSeat };
